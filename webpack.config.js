fa1d7797c868d4a9574db0264c2f42763621e3e4
Marcelo Vargas <marcelo_vargas@gap.com>
2016-02-15 23:50:25 -0200

    entry file app moved to app root dir. created dist folder to be used as bundle folder. moved html file to dist folder. changed webpack to expected served files in dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   context: __dirname + '/app',
-  entry: './scripts/app.js',
+  entry: './app.js',
   output: {
+    path: __dirname + '/dist', 
     filename: 'bundle.js'
   }
 };
